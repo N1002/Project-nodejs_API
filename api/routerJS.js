60c22482ebb571797fec6357bbdf819363ff90a5
Nam <107612309+N1002@users.noreply.github.com>
2022-07-18 22:45:38 +0700

    Update routerJS.js
    update tên file :))

--- api/routerJS.js
+++ api/routerJS.js
@@ -4,10 +4,10 @@ const router = express.Router(); // Lấy Class Router hoặc dùng mỗi expres
 //////////////////////
 //* SETUP PATH API *//
 //////////////////////
-const path_api = require("./js/api.js"); // Yêu cầu sử dụng file trong đường dẫn bạn cung cấp.
+const path_api = require("./controllersJS/api.js"); // Yêu cầu sử dụng file trong đường dẫn bạn cung cấp.
 router.get("/regkey", path_api.registerkey); // Thay đổi đường dẫn API và chọn hàm cần chạy code khi người dùng vào đường dẫn API đã ghi.
 
-const path_ip = require("./js/ip.js");
+const path_ip = require("./controllersJS/checkip.js");
 router.get("/checkip", path_ip.checkip);
 
 module.exports = router; 
