d74dd38e4dce32bce9bb0219ca55c12c8c744f0e
Nam <107612309+N1002@users.noreply.github.com>
2022-07-18 21:53:21 +0700

    routerJS.js
    SETUP PATH URL API

---
+++ api/routerJS.js
@@ -0,0 +1,13 @@
+const express = require("express"); // Yêu cầu sử dụng file trong node_module.
+const router = express.Router(); // Lấy Class Router hoặc dùng mỗi express() cũng được.
+
+//////////////////////
+//* SETUP PATH API *//
+//////////////////////
+const path_api = require("./js/api.js"); // Yêu cầu sử dụng file trong đường dẫn bạn cung cấp.
+router.get("/regkey", path_api.registerkey); // Thay đổi đường dẫn API và chọn hàm cần chạy code khi người dùng vào đường dẫn API đã ghi.
+
+const path_ip = require("./js/ip.js");
+router.get("/checkip", path_ip.checkip);
+
+module.exports = router; 
