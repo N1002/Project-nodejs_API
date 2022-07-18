6d92611b54804436f03978c9e8f01da73f646638
Nam <107612309+N1002@users.noreply.github.com>
2022-07-18 22:44:22 +0700

    Update routerHTML.js
    ...

--- api/routerHTML.js
+++ api/routerHTML.js
@@ -3,15 +3,15 @@ const path = require("path"); // Yêu cầu file từ node_module.
 const router = express.Router(); // Lấy Class Router hoặc mỗi express() cũng được.
 
 router.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/html/home.html"));
+    res.sendFile(path.join(__dirname + "/controllersHTML/home.html"));
 }); // Chạy file html
 
 router.get("/converbase64", function(req, res) {
-    res.sendFile(path.join(__dirname + "/html/converbase64.html"));
+    res.sendFile(path.join(__dirname + "/controllersHTML/converbase64.html"));
 });
 
 router.get("/clock", function(req, res) {
-    res.sendFile(path.join(__dirname + "/html/clock.html"));
+    res.sendFile(path.join(__dirname + "/controllersHTML/clock.html"));
 });
 
 module.exports = router;
